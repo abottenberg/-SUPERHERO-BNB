7ddbd3374182f0dad2b2eb517fdbf10fb50eddbe
Daniel Turkington <turkington.ad@gmail.com>
2020-05-19 14:43:32 +0100

    Added edit and update method and view for Supers

--- app/controllers/supers_controller.rb
+++ app/controllers/supers_controller.rb
@@ -1,5 +1,5 @@
 class SupersController < ApplicationController
-  before_action :set_super, only: [:show]
+  before_action :set_super, only: [:show, :edit, :update]
 
   def index
       if params[:superhero].present?
@@ -11,17 +11,32 @@ class SupersController < ApplicationController
       else
         @supers = Super.all
       end
-    end
-  
+  end
+
   def show
   end
-  
+
   def new
     @user = current_user
     @super = Super.new
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @super.update(super_params)
+      redirect_to @super, notice: 'Superhero was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def super_params
+    params.require(:super).permit(:name, :location, :good, :photos)
+  end
 
   def set_super
     @super = Super.find(params[:id])
