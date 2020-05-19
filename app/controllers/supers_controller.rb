4132425a092995d55b80ead2e28dc1d09e281299
Daniel Turkington <turkington.ad@gmail.com>
2020-05-19 13:07:40 +0100

    Created show action in super controller and show view page

--- app/controllers/supers_controller.rb
+++ app/controllers/supers_controller.rb
@@ -1,2 +1,12 @@
 class SupersController < ApplicationController
+  before_action :set_super, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_super
+    @super = Super.find(params[:id])
+  end
 end
