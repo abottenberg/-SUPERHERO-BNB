6e658fa5bf3c403685b0a8b414f8e41713bb7899
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-05-19 15:13:56 +0100

    changed booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :supers do
-    resources :bookings
+    resources :bookings, only: [:new, :create, :edit, :update, :destroy]
   end
 end
