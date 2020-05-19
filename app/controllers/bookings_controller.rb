8d0da3a6eee04b9da550c1a9435ed2977f1951fb
Naomi Kelly <naomigk13@gmail.com>
2020-05-19 14:24:19 +0100

    Added a new and create method to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def new
+    @bookings = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+     else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user, :super)
+  end
 end
